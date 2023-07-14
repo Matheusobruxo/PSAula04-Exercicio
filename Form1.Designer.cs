13b6dcccfe74ee063a2792cfbc9941d3274efa8c
Matheus Silva <41930738+Matheusobruxo@users.noreply.github.com>
2023-07-13 21:35:47 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PSAula04_Exercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBotaoVermelho = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblClicadas = new System.Windows.Forms.Label();
+            this.lblMultiplicador = new System.Windows.Forms.Label();
+            this.btnSorteio = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBotaoVermelho)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBotaoVermelho
+            // 
+            this.btnBotaoVermelho.Image = ((System.Drawing.Image)(resources.GetObject("btnBotaoVermelho.Image")));
+            this.btnBotaoVermelho.Location = new System.Drawing.Point(172, 121);
+            this.btnBotaoVermelho.Name = "btnBotaoVermelho";
+            this.btnBotaoVermelho.Size = new System.Drawing.Size(267, 254);
+            this.btnBotaoVermelho.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnBotaoVermelho.TabIndex = 0;
+            this.btnBotaoVermelho.TabStop = false;
+            this.btnBotaoVermelho.Click += new System.EventHandler(this.btnBotaoVermelho_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(133, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aperte isso!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aperte isso varias vezes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(496, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Porque?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(434, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Sei lá";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(27, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 30);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Mas por";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(-5, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 30);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Descobre e";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(49, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 30);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "me conta";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(40, 375);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 30);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Enquanto";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(160, 393);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(306, 67);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Aperte isso!";
+            // 
+            // lblClicadas
+            // 
+            this.lblClicadas.AutoSize = true;
+            this.lblClicadas.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClicadas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblClicadas.Location = new System.Drawing.Point(18, 134);
+            this.lblClicadas.Name = "lblClicadas";
+            this.lblClicadas.Size = new System.Drawing.Size(104, 30);
+            this.lblClicadas.TabIndex = 10;
+            this.lblClicadas.Text = "Clicadas:";
+            // 
+            // lblMultiplicador
+            // 
+            this.lblMultiplicador.AutoSize = true;
+            this.lblMultiplicador.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplicador.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMultiplicador.Location = new System.Drawing.Point(404, 193);
+            this.lblMultiplicador.Name = "lblMultiplicador";
+            this.lblMultiplicador.Size = new System.Drawing.Size(153, 30);
+            this.lblMultiplicador.TabIndex = 11;
+            this.lblMultiplicador.Text = "Multiplicador:";
+            // 
+            // btnSorteio
+            // 
+            this.btnSorteio.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnSorteio.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btnSorteio.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSorteio.Location = new System.Drawing.Point(462, 253);
+            this.btnSorteio.Name = "btnSorteio";
+            this.btnSorteio.Size = new System.Drawing.Size(114, 37);
+            this.btnSorteio.TabIndex = 12;
+            this.btnSorteio.Text = "Sorteio";
+            this.btnSorteio.UseVisualStyleBackColor = false;
+            this.btnSorteio.Click += new System.EventHandler(this.btnSorteio_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label11.Location = new System.Drawing.Point(487, 293);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(59, 16);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "1 - 1000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(599, 479);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnSorteio);
+            this.Controls.Add(this.lblMultiplicador);
+            this.Controls.Add(this.lblClicadas);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBotaoVermelho);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Aperte isso!";
+            ((System.ComponentModel.ISupportInitialize)(this.btnBotaoVermelho)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btnBotaoVermelho;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblClicadas;
+        private System.Windows.Forms.Label lblMultiplicador;
+        private System.Windows.Forms.Button btnSorteio;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSAula04_Exercicio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int clicadas;
+        public static int multiplicador = 1;
+        Random sorteador = new Random();
+
+        private void btnBotaoVermelho_Click(object sender, EventArgs e)
+        {
+            clicadas = clicadas + (1 * multiplicador);
+            lblClicadas.Text = $"Clicadas: {clicadas}";
+        }
+
+        private void btnSorteio_Click(object sender, EventArgs e)
+        {
+            multiplicador = sorteador.Next(0,1000);
+            lblMultiplicador.Text = $"Multiplicador: {multiplicador}";
+        }
+    }
+}
